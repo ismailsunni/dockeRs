4ed5f159d5f2afe9ced49d2c9a8bfdf325b58c08
Ismail Sunni <imajimatika@gmail.com>
2019-11-07 09:16:09 +0100

    More dep for arch, centos, debian.

--- scripts/dep_generator.R
+++ scripts/dep_generator.R
@@ -6,68 +6,84 @@ spatiotemporal_packages <- readLines("./scripts/spatiotemporal_packages.txt")
 all_packages = sort(unique(c(spatial_packages, spatiotemporal_packages)))
 unused_package = c('lgcp')
 all_packages = setdiff(all_packages, unused_package)
+pardir = "./scripts/sysreqs/"
 # spatial_packages
 # spatiotemporal_packages
 
 # manually set platform
-platform = "linux-x86_64-ubuntu-gcc"
+# platform = "linux-x86_64-ubuntu-gcc"
+# platform = "linux-x86_64-fedora-gcc"
 
-# dep <- sysreqs(desc = file.path(path.package(package = "sf"), "DESCRIPTION"), platform=platform)
-# Initialization
-all_deps = c()
-# failed packages file
-file.create(paste0("./scripts/", platform, "-failed-packages.txt"))
-i = 0
-for (p in all_packages){
-    i = i + 1
-    print(paste0("[",  i, "/", length(all_packages), "] ", p))
-    tryCatch(
-        {
-            library(p, character.only=TRUE)
-            dep <- sysreqs(desc = file.path(path.package(package = p), "DESCRIPTION"), platform=platform)
-            print(paste0("Number of dep: ", length(dep)))
-            print(paste0("Number of unique dep: ", length(unique(dep))))
-            # print(sort(dep))
-            all_deps <- c(all_deps, dep)
-            # print(all_deps)
-        },
-        error=function(error_message) {
-            message(paste0("Error message from R for : "), p)
-            message(error_message)
-            # update failed packages file
-            failed_packages <- readLines(paste0("./scripts/", platform, "-failed-packages.txt"))
-            failed_packages <- sort(c(failed_packages, p))
-            failed_packages_file <- file(paste0("./scripts/", platform, "-failed-packages.txt"))
-            writeLines(failed_packages, failed_packages_file)
-            close(failed_packages_file)
+platforms = c(
+    # "linux-x86_64-arch-gcc",
+    # "linux-x86_64-centos6-epel",
+    # "linux-x86_64-debian-clang",
+    "linux-x86_64-debian-gcc",
+    "linux-x86_64-fedora-clang",
+    "linux-x86_64-fedora-gcc",
+    "linux-x86_64-ubuntu-gcc"
+)
+for (platform in platforms){
+    # dep <- sysreqs(desc = file.path(path.package(package = "sf"), "DESCRIPTION"), platform=platform)
+    # Initialization
+    all_deps = c()
+    # failed packages file
+    file.create(paste0(pardir, platform, "-failed-packages.txt"))
+    i = 0
+    for (p in all_packages){
+        i = i + 1
+        print(paste0("[",  i, "/", length(all_packages), "] ", platform, " ", p))
+        tryCatch(
+            {
+                library(p, character.only=TRUE)
+                dep <- sysreqs(desc = file.path(path.package(package = p), "DESCRIPTION"), platform=platform)
+                print(paste0("Number of dep: ", length(dep)))
+                print(paste0("Number of unique dep: ", length(unique(dep))))
+                # print(sort(dep))
+                all_deps <- c(all_deps, dep)
+                # print(all_deps)
+            },
+            error=function(error_message) {
+                message(paste0("Error message from R for : "), p)
+                message(error_message)
+                # update failed packages file
+                failed_packages <- readLines(paste0(pardir, platform, "-failed-packages.txt"))
+                failed_packages <- sort(c(failed_packages, p))
+                failed_packages_file <- file(paste0(pardir, platform, "-failed-packages.txt"))
+                writeLines(failed_packages, failed_packages_file)
+                close(failed_packages_file)
+            }
+        )
+        if (i %% 10 == 0){
+            # Write result cache here
+            print("Writing cache")
+            if (length(all_deps) > 0){
+                all_deps <- sort(unique(all_deps))
+                dep_file_cache <- file(paste0(pardir, platform, "-deps-cache.txt"))
+                writeLines(all_deps, dep_file_cache)
+                close(dep_file_cache)
+            }
+            
         }
-    )
-    if (i %% 10 == 0){
-        # Write result cache here
-        print("Writing cache")
-        all_deps <- sort(unique(all_deps))
-        dep_file_cache <- file(paste0("./scripts/", platform, "deps-cache.txt"))
-        writeLines(all_deps, dep_file_cache)
-        close(dep_file_cache)
     }
-}
-all_deps <- sort(unique(all_deps))
-print(paste0("Total dep : ", length(all_deps)))
+    all_deps <- sort(unique(all_deps))
+    print(paste0("Total dep : ", length(all_deps)))
 
-# print(all_deps)
-# Write list of package to file
-dep_file <- file(paste0("./scripts/", platform, "-deps.txt"))
-if (length(all_deps) > 0){
-    writeLines(all_deps, dep_file)
-}
-close(dep_file)
+    # print(all_deps)
+    # Write list of package to file
+    dep_file <- file(paste0(pardir, platform, "-deps.txt"))
+    if (length(all_deps) > 0){
+        writeLines(all_deps, dep_file)
+    }
+    close(dep_file)
 
-# Failed packages
-failed_packages <- readLines(paste0("./scripts/", platform, "-failed-packages.txt"))
-if (length(failed_packages) > 0){
-    print("Failed packages are: ")
-    print(failed_packages)
-} else {
-   print("No failed packages.")
-}
+    # Failed packages
+    failed_packages <- readLines(paste0(pardir, platform, "-failed-packages.txt"))
+    if (length(failed_packages) > 0){
+        print("Failed packages are: ")
+        print(failed_packages)
+    } else {
+    print("No failed packages.")
+    }
 
+}

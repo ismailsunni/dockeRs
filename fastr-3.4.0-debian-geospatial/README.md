f5d4e404f852463ec89e2efd56f1c386596a8b60
Ismail Sunni <imajimatika@gmail.com>
2019-11-20 14:57:43 +0100

    Add FastR on geospatial.

---
+++ fastr-3.4.0-debian-geospatial/README.md
@@ -0,0 +1,6 @@
+# FastR 3.4.0 on Debian
+
+There is already a docker image for this combination:
+
+- [Source](https://github.com/nuest/fastr-docker)
+- [Docker Image](https://hub.docker.com/r/nuest/fastr)
---
+++ fastr-3.4.0-debian-geospatial/scripts/check_packages.R
@@ -0,0 +1,39 @@
+print("Checking Spatial and SpatioTemporal packages completeness")
+
+# Ger list of available packages
+spatial_packages <- readLines("./spatial_packages.txt")
+spatiotemporal_packages <- readLines("./spatiotemporal_packages.txt")
+
+# Get current installed pcakges
+installed_packages <- installed.packages()[ ,1]
+
+spatial_not_installed <- setdiff(spatial_packages, installed_packages)
+spatiotemporal_not_installed <- setdiff(spatiotemporal_packages, installed_packages)
+
+if (length(spatial_not_installed) == 0){
+    print("All Spatial R packages are installed.")
+} else {
+    print("Missing Spatial R packages are:")
+    print(spatial_not_installed)
+}
+
+spatial_number_installed = length(spatial_packages) - length(spatial_not_installed)
+print(paste("Installed packages:", spatial_number_installed))
+print(paste("Not installed packages:", length(spatial_not_installed)))
+print(paste("Total packages:", length(spatial_packages)))
+print(paste("Installed percentage:", round(spatial_number_installed / length(spatial_packages) * 10000) / 100, "%"))
+
+print("")
+
+if (length(spatiotemporal_not_installed) == 0){
+    print("All SpatioTemporal  R packages are installed.")
+} else {
+    print("Missing SpatioTemporal R packages are:")
+    print(spatiotemporal_not_installed)
+}
+
+spatiotemporal_number_installed = length(spatiotemporal_packages) - length(spatiotemporal_not_installed)
+print(paste("Installed packages:", spatiotemporal_number_installed))
+print(paste("Not installed packages:", length(spatiotemporal_not_installed)))
+print(paste("Total packages:", length(spatiotemporal_packages)))
+print(paste("Installed percentage:", round(spatiotemporal_number_installed / length(spatiotemporal_packages) * 10000) / 100, "%"))

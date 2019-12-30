76105f3d27f64fc825fd768fb2da541e659fce07
Ismail Sunni <imajimatika@gmail.com>
2019-12-30 13:50:34 +0100

    Add list of core packages.

--- scripts/packages.R
+++ scripts/packages.R
@@ -9,7 +9,7 @@ print("Get Spatial and SpatioTemporal packages list")
 spatial_packages <- ctv:::.get_pkgs_from_ctv_or_repos("Spatial")[[1]]
 spatiotemporal_packages <- ctv:::.get_pkgs_from_ctv_or_repos("SpatioTemporal")[[1]]
 
-# Write list of package to file, for history
+# Write list of package to file
 spatial_file <- file("spatial_packages.txt")
 writeLines(spatial_packages, spatial_file)
 close(spatial_file)
@@ -17,3 +17,16 @@ close(spatial_file)
 spatiotemporal_file <- file("spatiotemporal_packages.txt")
 writeLines(spatiotemporal_packages, spatiotemporal_file)
 close(spatiotemporal_file)
+
+# Get list of available CORE packages
+core_spatial_packages <- ctv:::.get_pkgs_from_ctv_or_repos("Spatial", coreOnly=TRUE)[[1]]
+core_spatiotemporal_packages <- ctv:::.get_pkgs_from_ctv_or_repos("SpatioTemporal", coreOnly=TRUE)[[1]]
+
+# Write list of package to file
+core_spatial_file <- file("core_spatial_packages.txt")
+writeLines(core_spatial_packages, core_spatial_file)
+close(core_spatial_file)
+
+core_spatiotemporal_file <- file("core_spatiotemporal_packages.txt")
+writeLines(core_spatiotemporal_packages, core_spatiotemporal_file)
+close(core_spatiotemporal_file)
